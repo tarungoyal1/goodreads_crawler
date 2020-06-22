37cb828df9df5af8ec0176ec2e760d1b5d107fa3
Tarun Goyal <tarun13317@gmail.com>
2020-06-22 13:29:20 +0530

    minor bug fixes

--- runallspiders.py
+++ runallspiders.py
@@ -1,4 +1,3 @@
-import scrapy
 from scrapy.crawler import CrawlerProcess
 from goodreads_cralwer.spiders.singlelistscrape import SinglelistcrawlSpider
 from goodreads_cralwer.spiders.get_book_details import GetBookDetailsSpider
